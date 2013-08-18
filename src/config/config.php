1734d17079a53d172371f70445fe734de56ce6ac
Terry Appleby <terry.appleby@gmail.com>
2013-08-18 14:57:05 -0400

    Whitespace tweaks.

--- src/config/config.php
+++ src/config/config.php
@@ -5,23 +5,30 @@ return array(
 	/**
 	 * Bindings for the various repositories.
 	 */
+
 	'repositories' => array(
+
 		/**
 		 * Bind string which will be automaticallty resolved.
 		 */
+
 		'client_credentials' => '\Tappleby\OAuth2\Repositories\ClientCredentialsRepositoryEloquent',
+
 
 		/**
 		 * The redis repositories have dependencies that cannot be resolved automatically, can define a closure instead.
 		 */
+
 		'access_token' => function($app) {
 			$redis = $app['redis'];
 			return new \Tappleby\OAuth2\Repositories\AccessTokenRepositoryRedis( $redis );
 		},
 
+
 		/**
 		 * The redis repositories have dependencies that cannot be resolved automatically, can define a closure instead.
 		 */
+
 		'refresh_token' => function($app) {
 			$redis = $app['redis'];
 			return new \Tappleby\OAuth2\Repositories\RefreshTokenRepositoryRedis( $redis );
@@ -31,6 +38,7 @@ return array(
 	/**
 	 * Raw configuration for oauth server.
 	 */
+
 	'server' => array(
 		'access_lifetime'            => 3600,
 		'www_realm'                  => 'Service',
